eb4ccbc74eb571233ff68e856babe2452e2d17ca
rayan <rayanfayadhussein>
2023-06-03 15:46:05 +0300

    edit front3

--- src/pages/Home/training.js
+++ src/pages/Home/training.js
@@ -18,20 +18,9 @@ function Training() {
         <div className="training-text">
           <div>
             <p className="training">
-            Your contribution can make a difference in someone's life.
+            Your contribution can make a difference in someone's life you can 
     Donate today and help us create positive change in our community.
-    Together, we can build a brighter future through your generous support.
-    By donating, you can help provide essential resources to those in need.
-    Your generosity can transform lives and inspire hope.
-    Every donation counts, no matter how big or small.
-    Join us in our mission to make a meaningful impact through your contribution.
-    With your support, we can make a lasting difference in the lives of countless individuals.
-    Your donation will directly support our efforts to create a better world.
-    Together, let's empower those who are less fortunate and create a more equitable society.
-    Your kindness can bring joy and relief to those facing difficult circumstances.
-    Donate today and be a catalyst for positive change in our community.
-   Your financial support can help us continue our vital work and reach even more people in need.
-
+    Together, we can build a brighter future through your generous support.By donating, you can help provide essential resources to those in need.Your generosity can transform lives and inspire hope.Every donation counts, no matter how big or small.Join us in our mission to make a meaningful impact through your contribution. With your support, we can make a lasting difference in the lives of countless individuals.Your donation will directly support our efforts to create a better world.Together, let's empower those who are less fortunate and create a more equitable society.Donate today and be a catalyst for positive change in our community.
 Please let me know if you need any further assistance or if there's anything else I can help you with!
             </p>
             {/* <Link to="/training">
