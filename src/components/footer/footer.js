67ce0aa7c15fe4f313a9b1a7eeb37cc668fc17b2
rayan <rayanfayadhussein>
2023-06-05 14:49:06 +0300

    edit finalf

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -67,7 +67,7 @@ const Footer = () => {
           <div className="footer-responsive-bottom-side">
             <div className="footer-social-media">
               <h3>Follow Us</h3>
-              <ul style={{display:"flex", justifyContent:"space-around"}}>
+              <ul style={{display:"flex", gap: '1rem'}}>
                 <li>
                   <a
                     href="https://www.facebook.com/profile.php?id=100092553177679"
